1f729c1d0c3645997f109a3cc366daac9b23e83e
Maha-Sidia <sidiamaha43@gmail.com>
2025-11-03 21:18:08 +0100

    first commit back-end

---
+++ src/api/post/middlewares/populate-posts.ts
@@ -0,0 +1,14 @@
+/**
+ * `populate-posts` middleware
+ */
+
+import type { Core } from '@strapi/strapi';
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info('In populate-posts middleware.');
+
+    await next();
+  };
+};

16d6c51c30659cdbf1b75ef407ba16b89e2c8412
Maha-Sidia <sidiamaha43@gmail.com>
2025-11-07 19:26:02 +0100

    fix config/middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,30 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://projet-my-blog-1yvi.vercel.app'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://projet-my-blog-1yvi.vercel.app'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://projet-my-blog-1yvi.vercel.app'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
